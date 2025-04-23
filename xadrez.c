f5f4e0458af8d1bdecf55a62b98c54a5231c06b1
AMCGUIMA <anderson.m_guimaraes@hotmail.com>
2025-04-22 22:44:09 -0300

    NIVEL MESTRE CONCLUIDO

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,54 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+
+//FUNÇÃO RECURSIVA PARA IMPRIMIR O MOVIMENTO DA TORRE
+void recursiveTorre(int t){
+    if(t > 0){
+
+        printf("Direita\n"); //IMPRIME O MOVIMENTO DA TORRE
+
+        recursiveTorre(t - 1); //CHAMA A PROPRIA FUNÇÃO COM t - 1
+
+    }
+}
+
+
+
+recursiveBispo(int b){
+    if(b > 0){
+
+       for(int j = 0; j < 1; j++)
+       {
+            for (int i = 0; i < 1; i++)
+            {
+                printf("Cima, ");
+            }
+
+            printf("Direita\n");
+            
+        }
+
+       recursiveBispo(b - 1);
+        
+    }
+
+}
+
+//FUNÇÃO RECURSIVA PARA IMPRIMIR O MOVIMENTO DA RAINHA
+recursiveRainha(int r){
+    if(r > 0){
+
+        printf("Direita\n"); //IMPRIME O MOVIMENTO DA RAINHA
+
+        recursiveTorre(r - 1); //CHAMA A PROPRIA FUNÇÃO COM r - 1
+    }
+}
+
+
+
+int main() 
+{
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -29,54 +76,40 @@ int main() {
     // Inclua o uso de continue e break dentro dos loops.
 
 //DECLARAÇÃO DAS VARIAVES RESPONSAVEIS POR ARMAZENAR A QUANTIDADE DE MOVIMENTO DAS PEÇAS DO XADREX
-int torre = 5;
-int bispo = 5;
-int rainha = 8;
+    int torre = 5;
+    int bispo = 5;
+    int rainha = 8;
 
-int aux = 1; // VARIAVEL AUXILIAR PARA TRATAR OS LOOP WHILE E DO WHILE.
+    //TRECHO RESPONSAVEL PELO MOVIMENTO DA TORRE.
+    printf("Torre:\n");
+    recursiveTorre(torre); //CHAMA A FUNÇÃO recursiveTorre() como o valor torre para tratamento
 
-//TRECHO RESPONSAVEL PELO MOVIMENTO DA TORRE.
-printf("\n\nTORRE:\n");
-for (int i = 1; i <= torre; i++) // TRECHO DO CODIGO RESPONSAVEL POR INCREMENTAR O MOVIMENTO DA TORRE USANDO O LOOP for.
-{
-    printf("Direita\n");// SAIDA DE DADOS, RESULTADO DO INCREMENTO EXECUTADO DENTRO DO LAÇO for.
-}
+    //TRECHO RESPONSAVEL PELO MOVIMENTO DA RAINHA.
+    printf("\n\nRAINHA:\n");
+    recursiveRainha(rainha);
 
-//TRECHO RESPONSAVEL PELO MOVIMENTO DA BISPO.
-printf("\n\nBISPO:\n");
-while(aux <= bispo) // TRECHO DO CODIGO RESPONSAVEL POR INCREMENTAR O MOVIMENTO DA TORRE USANDO O LOOP while.
-{
-    printf("Cima, Direita\n");// SAIDA DE DADOS, RESULTADO DO INCREMENTO EXECUTADO DENTRO DO LAÇO while.
-    aux++; // variavel auxiliar criada para guardar o incremento para ser usado na condição while
+    printf("\n");// IMPRIME UM LINHA EM BRANCO
 
-}
 
-//TRECHO RESPONSAVEL PELO MOVIMENTO DA RAINHA.
-aux = 1;
-printf("\n\nRAINHA:\n");
-do  // TRECHO DO CODIGO RESPONSAVEL POR INCREMENTAR O MOVIMENTO DA TORRE USANDO O LOOP do while.
-{
-    printf("Esquerda\n"); // SAIDA DE DADOS, RESULTADO DO INCREMENTO EXECUTADO DENTRO DO LAÇO do while.
-    aux++; // variavel auxiliar criada para guardar o incremento para ser usado na condição do while
+    //TRECHO RESPONSAVEL PELO MOVIMENTO DA BISPO.
+    printf("\n\nBISPO:\n");
+    recursiveBispo(bispo);
 
-} while (aux <= rainha);//codição do While para executar o codigo.
+    printf("\n");
 
-//MOVIMENTO DO CAVALO -- NIVEL AVENTUREIRO
-aux = 0; //VARIAVEL AUXILIAR PARA TRATAR A CONDIÇAO DO LOOP WHILE
-printf("\n"); //IMPRIMI UMA LINHA EM BRANCO PARA SEPARAR OS MOVIMENTOS DO CAVALO DAS OUTRAS PEÇAS
-printf("\nCAVALO: \n");
+    //MOVIMENTO DO CAVALO 
+    printf("\n"); //IMPRIMI UMA LINHA EM BRANCO PARA SEPARAR OS MOVIMENTOS DO CAVALO DAS OUTRAS PEÇAS
+    printf("\nCAVALO: \n");
 
-while (aux < 1)
-{
-    for (int i = 1; i <= 2; i++)
-    {
-        printf("Baixo, ");// SAIDA DE DADOS, RESULTADO DO INCREMENTO EXECUTADO DENTRO DO LAÇO FOR. IMPRIMI NO CONSOLE "Baixo, " duas vezes
+
+    for (int i = 1; i <= 3; i++)//LOOP COM A QUANTIDADE DE MOVIMENTOS DO CAVALO
+    {   
+        if( i == 2) continue; //ASSIM QUE MOVIMENTA DUAS CASAS UTILIZO OCONTROLE FLUXO continue PARA FINALIZAR O TERCEIRO MOVIMENTO PARA DIRETA FORA DO LOOP
+
+        printf("Cima, ");// SAIDA DE DADOS, RESULTADO DO INCREMENTO EXECUTADO DENTRO DO LAÇO FOR. IMPRIMI NO CONSOLE "CIMA, " duas vezes
     }
 
-    printf("Esquerda");// ASSIM QUE TERMINA O LOOP FOR IMPRIMI NA SEQUENCIA PARA "ESQUERDA"
-    aux++; //INCRIMENTA +1 NA VARIAVEL AUXILIAR PARA FINALIZAR A CONDICÇÃO DO WHILE
-    
-}
-
+        printf("Direita");// ASSIM QUE TERMINA O LOOP FOR IMPRIMI NA SEQUENCIA PARA "DIREITA" UMA VEZ
+        
     return 0;
 }
